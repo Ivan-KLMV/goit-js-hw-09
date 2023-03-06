11c440ece561c3d04217f0194b4528b942bb0595
Ivan-KLMV <klimov128@gmail.com>
2023-03-06 14:30:41 +0000

    Deploying to gh-pages from @ Ivan-KLMV/goit-js-hw-09@450d3df3e2512342e5235fc057624e67ba4a81b0 🚀

---
+++ 01-color-switcher.815162ae.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");function d(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.setAttribute("disabled",!0),t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),d(),timerId=setInterval((()=>{d()}),1e3)})),e.addEventListener("click",(function(){e.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.815162ae.js.map
